878e6592b33dc02cdd6fda5d7b60bc293e233195
Ionut Bostan <bostanionut28@gmail.com>
2021-11-23 16:40:17 +0000

    Added price list & more validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,16 +6,21 @@ class Reservation < ApplicationRecord
   validates :bay_type, presence: true
   validates :vehicle_registration, presence: true, length: {minimum: 6}, format: { with: /\A[0-9a-zA-Z]+\z/,
     message: "only allows letters and numbers" }
-  validates :duration, presence: true, numericality: {greater_than_or_equal_to: 1}
-
+  validates :duration, presence: true, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 24, only_integer: true}
   scope :user_reservations, ->(user) { where(['user_id = ?', user.id]) }
 
   before_save :calculate_price
 
   def calculate_price
-    self.price = 2.5
-    value = duration * price
-    self.price = value
+    if duration == 1
+      self.price = 1.2
+    elsif duration > 1 && duration <= 2
+      self.price = 2
+    elsif duration > 2 && duration <= 4
+      self.price = 3.6
+    else
+      self.price = 5.7
+    end
   end
 
 end
