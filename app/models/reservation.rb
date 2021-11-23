ee073902d92b6e3a9343524c627609146a150896
Ionut Bostan <bostanionut28@gmail.com>
2021-11-23 15:53:05 +0000

    Added  extra validation to form fields

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,8 +4,9 @@ class Reservation < ApplicationRecord
 
   validates :location, presence: true
   validates :bay_type, presence: true
-  validates :vehicle_registration, presence: true
-  validates :duration, presence: true
+  validates :vehicle_registration, presence: true, length: {minimum: 6}, format: { with: /\A[0-9a-zA-Z]+\z/,
+    message: "only allows letters and numbers" }
+  validates :duration, presence: true, numericality: {greater_than_or_equal_to: 1}
 
   scope :user_reservations, ->(user) { where(['user_id = ?', user.id]) }
 
