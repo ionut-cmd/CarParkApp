92f22c71a2c281469d5895e1366f0c7dfffe04ca
Ionut Bostan <bostanionut28@gmail.com>
2021-12-04 15:59:17 +0000

    Added admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_101319) do
+ActiveRecord::Schema.define(version: 2021_12_04_155453) do
 
   create_table "carparks", force: :cascade do |t|
     t.string "location", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_101319) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
