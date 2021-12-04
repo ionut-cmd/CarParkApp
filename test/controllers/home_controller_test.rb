892b73236a264f12479a56fb8695bb38826d1e2e
Ionut Bostan <bostanionut28@gmail.com>
2021-12-04 13:49:29 +0000

    Added pg gem

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  
+
   test "should get home" do
     get root_url
     assert_response :success
@@ -12,7 +12,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get contact_url
     assert_response :success
     assert_template layout: 'application'
-    # assert_select 'title', 'Car Park'
+    assert_select 'title', 'Car Park'
     assert_select 'h1', 'Contact Us'
     assert_select 'p', 'Complete to get in touch.'
   end
